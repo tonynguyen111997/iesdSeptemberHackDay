8a746e308ca4e35dae9d649264ad27f5b735d404
David <daveshuang@gmail.com>
2019-09-28 18:57:22 -0700

    Resolve conflict

--- routes/auth/router.js
+++ routes/auth/router.js
@@ -7,24 +7,7 @@ module.exports = app => {
 
   app.post("/dashboard", authController.getGitHubUser);
 
-<<<<<<< HEAD
-  app.post("/dashboard", (req, res) => {
-    const { username } = req.body;
-    axios.get(`https://api.github.com/users/${username}`).then(response => {
-      const { login, name, location, bio, avatar_url } = response.data;
-      const data = {
-        login,
-        name,
-        location,
-        bio,
-        avatar_url
-      };
-      res.json(data);
-    });
-  });
-=======
   app.get("/api/user/:username", authController.getUserInfo);
->>>>>>> c6b480c475dd5092f4a9f872ea8ff576a632ec1a
 
   app.get("/api/email/:userEmail", authController.getUserInfoEmail);
 };
